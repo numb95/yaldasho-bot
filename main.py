bc8ff60def093d019352edc8bbd99aa1e965545a
shervinmo <abtin@riseup.net>
2019-12-31 20:57:13 +0330

    fix /start bug
    no need to restart bot.
    
    fix pep8.
    
    no need one function for each design.

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 import configparser
 import logging
 from telegram.ext import Updater, CommandHandler
-from telegram import UserProfilePhotos, File, ChatAction, ParseMode, InlineKeyboardButton, InlineKeyboardMarkup
+from telegram import ChatAction, ParseMode
 from functools import wraps
-from time import sleep
 from PIL import Image
 from shutil import copyfile
 import os
+
 welcome_message = '''
 🍉🍉🍉🍉🍉🍉🍉🍉🍉🍉🍉🍉🍉 \n 
 🌹 سلام \n \n \n✳️این ربات با همکاری امیرحسین گودرزی به عنوان برنامه نویس و امین خلیقی به عنوان گرافیست و به منظور احیای شب یلدا و بهتر شدن حال هممون برای یه مدت زمانی طراحی شده.
@@ -23,6 +23,7 @@ wait_message = '''
 ⭐️تصویر نمایه شما آماده شده است. ربات در حال ارسال آن به شماست.⭐️
 '''
 
+
 def send_action(action):
     """Sends `action` while processing func command."""
 
@@ -30,56 +31,109 @@ def send_action(action):
         @wraps(func)
         def command_func(*args, **kwargs):
             bot, update = args
-            bot.send_chat_action(chat_id=update.effective_message.chat_id, action=action)
+            bot.send_chat_action(
+                chat_id=update.effective_message.chat_id,
+                action=action
+            )
             return func(bot, update, **kwargs)
         return command_func
-    
+
     return decorator
 
 
-def image_merge(profile_picture,banner_number):
+def image_merge(profile_picture, banner_number):
     im = Image.open(profile_picture, 'r')
     size = 640, 640
     thumbnail = im.resize(size)
     yalda = Image.open('assets/{}.png'.format(banner_number), 'r')
-    thumbnail.paste(yalda, (0,0), yalda)
+    thumbnail.paste(yalda, (0, 0), yalda)
     thumbnail.save(profile_picture, "JPEG")
 
+
 @send_action(ChatAction.TYPING)
-def start(bot , update):
+def start(bot, update):
     user_id = update.message.chat_id
-    user_name = update.message.chat.username
-    done_date = update.message.date
-    bot.send_message(chat_id=user_id, text= welcome_message,  parse_mode=ParseMode.MARKDOWN)
-    profile_picture_id = bot.getUserProfilePhotos(update.message.chat_id, 0).photos[0][-1].file_id
-    profile_picture_file = bot.get_file(profile_picture_id)
-    profile_picture_file.download('images/original/{}_@{}_original.jpg'.format(user_id,user_name))
-    copyfile('images/original/{}_@{}_original.jpg'.format(user_id,user_name), 'images/edited/{}_@{}_edited.jpg'.format(user_id,user_name))
-    bot.send_message(chat_id=user_id, text="دوتا طرح برات ارسال میشه. هرکدوم رو دوست داشتی انتخاب کن تا بر اساس اون تصویر نمایه‌ت ساخته بشه")
-    bot.send_photo(chat_id= update.message.chat_id, photo=open('assets/yalda1.jpg', 'rb',), caption='\n\n\n\n\n\n\n /design1 طرح اول')
-    bot.send_photo(chat_id= update.message.chat_id, photo=open('assets/yalda2.jpg', 'rb',), caption='\n\n\n\n\n\n\n /design2 طرح دوم')
+    bot.send_message(
+        chat_id=user_id,
+        text=welcome_message,
+        parse_mode=ParseMode.MARKDOWN
+    )
+    bot.send_message(
+        chat_id=user_id,
+        text="دوتا طرح برات ارسال میشه. هرکدوم رو دوست داشتی انتخاب کن تا بر اساس اون تصویر نمایه‌ت ساخته بشه"
+    )
+    bot.send_photo(
+        chat_id=update.message.chat_id,
+        photo=open('assets/yalda1.jpg', 'rb'),
+        caption='\n\n\n\n\n\n\n /design1 طرح اول'
+    )
+    bot.send_photo(
+        chat_id=update.message.chat_id,
+        photo=open('assets/yalda2.jpg', 'rb'),
+        caption='\n\n\n\n\n\n\n /design2 طرح دوم'
+    )
  
-   
-def design1(bot, update):
+
+def design(bot, update):
+    print(update)
+    pics = {
+        "/design1": "yalda1",
+        "/design2": "yalda2"
+    }
+    update.message.text
     user_id = update.message.chat_id
+    pic_name = pics[update.message.text]
     user_name = update.message.chat.username
-    done_date = update.message.date
-    image_merge('images/edited/{}_@{}_edited.jpg'.format(user_id,user_name),'yalda1')
+    profile_picture_id = bot.getUserProfilePhotos(
+        update.message.chat_id, 0).photos[0][-1].file_id
+    profile_picture_file = bot.get_file(profile_picture_id)
+    profile_picture_file.download(
+            'images/original/{}_@{}_original.jpg'.format(
+                user_id,
+                user_name
+            )
+    )
+    copyfile(
+        'images/original/{}_@{}_original.jpg'.format(
+            user_id,
+            user_name
+        ),
+        'images/edited/{}_@{}_edited.jpg'.format(
+            user_id,
+            user_name
+        )
+    )
+    image_merge(
+        'images/edited/{}_@{}_edited.jpg'.format(user_id, user_name),
+        pic_name
+    )
     bot.send_message(chat_id=update.message.chat_id, text=wait_message)
-    bot.send_photo(chat_id= update.message.chat_id, photo=open('images/edited/{}_@{}_edited.jpg'.format(user_id,user_name), 'rb'))
-    bot.send_message(chat_id=user_id, text="با تشکر. اگه مجدداً بخواین از ربات استفاده کنین دستور /start رو بزنین. این مورد رو بعداً باید به کد اضافه کنیم و باگ رو برطرف کنیم تا مشکلش برطرف بشه :)")
-    os.remove('images/edited/{}_@{}_edited.jpg'.format(user_id,user_name))
-    os.remove('images/original/{}_@{}_original.jpg'.format(user_id,user_name))
-def design2(bot, update):
-    user_id = update.message.chat_id
-    user_name = update.message.chat.username
-    done_date = update.message.date
-    image_merge('images/edited/{}_@{}_edited.jpg'.format(user_id,user_name),'yalda2')
-    bot.send_message(chat_id=update.message.chat_id, text=wait_message)
-    bot.send_photo(chat_id= update.message.chat_id, photo=open('images/edited/{}_@{}_edited.jpg'.format(user_id,user_name), 'rb'))
-    bot.send_message(chat_id=user_id, text="با تشکر. اگه مجدداً بخواین از ربات استفاده کنین دستور /start رو بزنین. این مورد رو بعداً باید به کد اضافه کنیم و باگ رو برطرف کنیم تا مشکلش برطرف بشه :)")
-    os.remove('images/edited/{}_@{}_edited.jpg'.format(user_id,user_name))
-    os.remove('images/original/{}_@{}_original.jpg'.format(user_id,user_name))
+    bot.send_photo(
+        chat_id=update.message.chat_id,
+        photo=open('images/edited/{}_@{}_edited.jpg'.format(
+                user_id,
+                user_name
+            ),
+            'rb'
+        )
+    )
+    bot.send_message(
+        chat_id=user_id,
+        text="اگه مجددا  بخواین از ربات استغاده کنید کافیه دوباره دستور  طرح مورد نظرتون رو وارد کنید."
+    )
+    os.remove(
+        'images/edited/{}_@{}_edited.jpg'.format(
+            user_id,
+            user_name
+        )
+    )
+    os.remove(
+        'images/original/{}_@{}_original.jpg'.format(
+            user_id,
+            user_name
+            )
+    )
+
 
 def main():
     logging.basicConfig(format='%(asctime)s - %(name)s - %(name)s - %(message)s', level=logging.INFO)
@@ -87,16 +141,16 @@ def main():
     config.read('config.ini')
     token = config['app-conf']['token']
     updater = Updater(token=token)
-    start_handler = CommandHandler ('start',start)
+    start_handler = CommandHandler('start', start)
     updater.dispatcher.add_handler(start_handler)
-    design1_handler = CommandHandler ('design1', design1)
+    design1_handler = CommandHandler('design1', design)
     updater.dispatcher.add_handler(design1_handler)
-    design2_handler = CommandHandler ('design2', design2)
+    design2_handler = CommandHandler('design2', design)
     updater.dispatcher.add_handler(design2_handler)
 
     updater.start_polling()
     updater.idle()
+
+
 if __name__ == '__main__':
     main()
-
-
